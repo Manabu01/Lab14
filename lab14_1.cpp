e897bce736d63e5b9b1efe8e68d0e41908908fe0
Poomipat <poomipatboonkong@gmail.com>
2026-01-09 17:10:09 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+   for(int i = 1; i < N; ++i){
+        T k = d[i];
+        int a = i;
+        int j = i - 1;
+        while (j >= 0 && d[j] < k){
+            d[a] = d[j];
+            d[j] = k;
+            k = d[j];
+            a = a - 1;
+            j = j - 1;
+        }
+        d[j+1] = k;
+        cout << "Pass " << i << ":";
+        for(int g = 0; g < N;g++) cout << d[g] << " ";
+        cout << "\n";
+   }
 }
 
 int main(){
